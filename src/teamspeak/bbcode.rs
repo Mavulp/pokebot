dbaaeb68bdf4143b9dca55b2baa86a2656a3249e
Felix Kaaman <tmtu@tmtu.ee>
2020-02-22 19:24:05 +0200

    Add functions for BBCode formatting

---
+++ src/teamspeak/bbcode.rs
@@ -0,0 +1,42 @@
+use std::fmt::{Formatter, Display, Error};
+
+#[allow(dead_code)]
+pub enum BbCode<'a> {
+    Bold(&'a dyn Display),
+    Italic(&'a dyn Display),
+    Underline(&'a dyn Display),
+    Link(&'a dyn Display, &'a str),
+}
+
+impl<'a> Display for BbCode<'a> {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+        match self {
+            BbCode::Bold(text) => fmt.write_fmt(format_args!("[B]{}[/B]", text))?,
+            BbCode::Italic(text) => fmt.write_fmt(format_args!("[I]{}[/I]", text))?,
+            BbCode::Underline(text) => fmt.write_fmt(format_args!("[U]{}[/U]", text))?,
+            BbCode::Link(text, url) => fmt.write_fmt(format_args!("[URL={}]{}[/URL]", url, text))?,
+        };
+
+        Ok(())
+    }
+}
+
+#[allow(dead_code)]
+pub fn bold(text: &dyn Display) -> BbCode {
+    BbCode::Bold(text)
+}
+
+#[allow(dead_code)]
+pub fn italic(text: &dyn Display) -> BbCode {
+    BbCode::Italic(text)
+}
+
+#[allow(dead_code)]
+pub fn underline(text: &dyn Display) -> BbCode {
+    BbCode::Underline(text)
+}
+
+#[allow(dead_code)]
+pub fn link<'a>(text: &'a dyn Display, url: &'a str) -> BbCode<'a> {
+    BbCode::Link(text, url)
+}
